77d264a4bedd4786af1a32813f67a66f9f70adcc
mohdShahwan <mshahwan002@gmail.com>
2023-07-19 17:05:22 +0300

    Using effects for fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <StarRating
-      defaultRating={3}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
+    <App />
   </React.StrictMode>
 );
+
+// <StarRating
+//   defaultRating={3}
+//   messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+// />
