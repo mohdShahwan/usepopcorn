fccac993791638bc85611e341a381f8fae2401d5
mohdShahwan <mshahwan002@gmail.com>
2023-07-21 21:01:26 +0300

    Added listener to 'enter' keypress in Search component

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,14 @@ export const KEY = "695851c1";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  // const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const [watched, setWatched] = useState(
+    JSON.parse(localStorage.getItem("watched"))
+  );
 
   function handleSelectMovie(id) {
     const newId = id === selectedId ? null : id;
@@ -41,6 +45,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
